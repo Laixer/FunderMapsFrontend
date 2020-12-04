7922afdee42f5b9660dab5518dfba4c79109c771
Patrick Nobbe <patrick.nobbe@hotmail.com>
2020-12-04 15:23:32 +0100

    Setup initial project architecture
    Adds Lerna, Yarn Workspaces, Babel, TypeScript + ESLint and Vue 3 support

--- packages/incidentportal/src/components/form/TextInput.vue
+++
@@ -1,7 +0,0 @@
-<script lang="ts">
-import { Component } from 'vue-property-decorator';
-import FormField from '@/components/common/FormField.vue'
-
-@Component
-export default class TextInput extends FormField {}
-</script>
