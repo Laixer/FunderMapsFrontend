e2d52f509ba0cd9cd645c6b088d063ed15cf073d
Patrick Nobbe <patrick.nobbe@hotmail.com>
2021-01-12 13:44:16 +0100

    Clean up base webpack config

--- vue.config.base.js
+++ vue.config.base.js
@@ -4,32 +4,46 @@ const path = require("path");
 const { argv } = require("yargs");
 
 const vendor = argv.vendor ? argv.vendor : "fundermaps";
-const vendorPath = path.relative('.', `../__vendor/src/${vendor}`);
+const vendorPath = path.relative(".", `../__vendor/src/${vendor}`);
 
-console.log("\x1b[32m \x1b[1m \x1b[5m", `\nAPP: \t${process.VUE_CLI_SERVICE.pkg.name}\nVENDOR: ${vendor}\nENV: \t${process.env.NODE_ENV}\n`, "\x1b[0m")
+console.log(
+  "\x1b[32m \x1b[1m \x1b[5m",
+  `\nAPP: \t${process.VUE_CLI_SERVICE.pkg.name}\nVENDOR: ${vendor}\nENV: \t${process.env.NODE_ENV}\n`,
+  "\x1b[0m"
+);
 
 exports.config = {
   plugin: {
     // TODO: Rename to project vendor
-    'vendor define': {
-      plugin: require('webpack').DefinePlugin,
+    "vendor define": {
+      plugin: require("webpack").DefinePlugin,
       args: [{ VENDOR: JSON.stringify(vendor) }]
     },
-    'vendor copy': {
-      plugin: require('copy-webpack-plugin'),
-      args: [{
-        patterns: [{
-          from: path.resolve(vendorPath, 'favicon.ico'),
-          to: "."
-        }]
-      }]
+    "vendor copy": {
+      plugin: require("copy-webpack-plugin"),
+      args: [
+        {
+          patterns: [
+            {
+              from: path.resolve(vendorPath, "favicon.ico"),
+              to: "."
+            }
+          ]
+        }
+      ]
     }
   },
   resolve: {
+    plugin: {
+      "tsconfig paths": {
+        plugin: require("tsconfig-paths-webpack-plugin")
+      }
+    },
     modules: [path.resolve(__dirname, vendorPath)]
   }
-}
+};
 
+console.log(path.resolve(__dirname, vendorPath));
 exports.css = {
   css: {
     loaderOptions: {
@@ -43,4 +57,4 @@ exports.css = {
       }
     }
   }
-}
+};
