5d77300b4f502f9eb320f6f2c3b2fc0b05ef4102
Patrick Nobbe <patrick.nobbe@hotmail.com>
2021-01-14 15:30:30 +0100

    Remove testing code

--- packages/customerportal/src/main.ts
+++ packages/customerportal/src/main.ts
@@ -1,11 +1,7 @@
 import App from "@/App.vue";
 import router from "@/router";
 import store from "@/store";
-import { FoundationType } from "@fundermaps/common";
 import { createApp } from "vue";
-
-const test: FoundationType = new FoundationType(FoundationType.Type.NoPile);
-console.log(test.translate());
 
 createApp(App)
   .use(store)
