efcca60a8fd6d49e905a78f4bc2d21714eb78a32
Patrick Nobbe <patrick.nobbe@hotmail.com>
2021-01-15 17:00:53 +0100

    Allow page title to be set on a per-vendor/app basis

--- packages/customerportal/src/App.vue
+++ packages/customerportal/src/App.vue
@@ -4,6 +4,18 @@
   </div>
 </template>
 
+<script lang="ts">
+import { defineComponent } from "vue";
+export default defineComponent({
+  name: "App",
+  watch: {
+    $route(to) {
+      document.title = to.meta.title || "Customerportal";
+    }
+  }
+});
+</script>
+
 <style lang="scss">
 body {
   @media only screen and (min-width: $BREAKPOINT) {
