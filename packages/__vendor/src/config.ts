968446175042ed1a5fa6aa80d984a08ee2cd09a4
Patrick Nobbe <patrick.nobbe@hotmail.com>
2021-01-15 17:00:14 +0100

    Update vendor configuration to allow for app namespaces

--- packages/__vendor/src/config.ts
+++ packages/__vendor/src/config.ts
@@ -1,25 +1,32 @@
 /* eslint-disable @typescript-eslint/no-var-requires */
 
-export declare const VENDOR: string;
+declare const VENDOR: string;
 
 export type VendorConfig = {
   name: string;
+  title: string;
   client_id: number;
   style: {
     mapboxPrimaryColor: string;
   };
-  background: string;
-  logo: string;
-  home: {
+  clientapp: {
     title: string;
-    subtitle: string;
-    content: string;
-    image: string;
   };
-  final: {
-    content: string;
+  customerportal: {
+    title: string;
+  };
+  incidentportal: {
+    title: string;
+    home: {
+      title: string;
+      subtitle: string;
+      content: string;
+      image: string;
+    };
+    final: {
+      content: string;
+    };
   };
 };
 
-const config: VendorConfig = require(`./${VENDOR}/config.json`);
-export default config;
+export const config: VendorConfig = require(`./${VENDOR}/config.json`);
